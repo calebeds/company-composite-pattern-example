ddb13430a28a624c13cf16bc0dc67e15b805876f
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-05-09 20:09:56 -0300

    change implementation to a hierarchy

--- src/Company.java
+++ src/Company.java
@@ -1,27 +1,21 @@
 import company.Developer;
-import company.Directory;
-import company.Employee;
 import company.Manager;
+import company.Employee;
 
 public class Company {
     public static void main(String[] args) {
         Employee dev1 = new Developer( "Calebe Oliveira", 100, "Pro Developer");
         Employee dev2 = new Developer( "Asaf Oliveira", 101, "Entry Level Developer");
 
-        Directory engDirectory = new Directory();
-        engDirectory.addEmployee(dev1);
-        engDirectory.addEmployee(dev2);
+        Manager engManager= new Manager("Mike", 2000);
+        engManager.addEmployee(dev1);
+        engManager.addEmployee(dev2);
 
-        Employee manager1 = new Manager("Fulana Da Silva", 200, "SEO Manager");
-        Employee manager2 = new Manager("Aquiles Oliveira", 201, "Myra's Manager");
+        Employee dev3 = new Developer("Tom", 102, "Developer");
 
-        Directory accDirectory = new Directory();
-        accDirectory.addEmployee(manager1);
-        accDirectory.addEmployee(manager2);
-
-        Directory company = new Directory();
-        company.addEmployee(engDirectory);
-        company.addEmployee(accDirectory);
-        company.showEmployeeDetails();
+        Manager generalManager = new Manager("Mark", 50000);
+        generalManager.addEmployee(dev3);
+        generalManager.addEmployee(engManager);
+        generalManager.showEmployeeDetails();
     }
 }
--- src/company/Manager.java
+++ src/company/Manager.java
@@ -1,17 +1,51 @@
 package company;
 
-public class Manager implements Employee {
-    private String name;
-    private long employeeId;
-    private String position;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-    public Manager(String name, long employeeId, String position) {
+public class Manager implements Employee {
+
+    private List<Employee> employeeList = new ArrayList<>();
+    private String name;
+    private double salary;
+
+    public Manager(String name, double salary) {
         this.name = name;
-        this.employeeId = employeeId;
-        this.position = position;
+        this.salary = salary;
     }
+
     @Override
     public void showEmployeeDetails() {
-        System.out.println(this.employeeId + " " + this.name + " " + this.position);
+        System.out.println("------------");
+        System.out.println("Name = " + this.getName());
+        System.out.println("Salary = " + this.getSalary());
+        System.out.println("------------");
+
+        Iterator<Employee> employeeIterator = employeeList.iterator();
+        while (employeeIterator.hasNext()) {
+            Employee employee = employeeIterator.next();
+            employee.showEmployeeDetails();
+        }
+    }
+
+    public Employee getChild(int i) {
+        return employeeList.get(i);
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
+    public void removeEmployee(Employee employee) {
+        employeeList.remove(employee);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
     }
 }
